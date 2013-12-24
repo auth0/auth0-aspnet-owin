5cd3c1dac58559c4e056db054d2486868897d292
Sebastian Iacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-12-24 13:38:18 -0300

    sample

---
+++ MvcSample/App_Start/Startup.Auth.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace MvcSample
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //   appId: "",
+            //   appSecret: "");
+
+            //app.UseGoogleAuthentication();
+
+            // add Google (with Auth0)
+            app.AddAuth0Authentication(
+                clientId:       "oHDqHR7JompKEQFHfwk1n0JYgbowVouo",
+                clientSecret:   "VkWbAzwvRIgwMybtUDsGSjIQYtKZajoeSzhDGVWDiMiU7Y4AScJfXy20TV2XIeY6", 
+                domain:         "iaco82.auth0.com", 
+                connection:     "google-oauth2", 
+                displayName:    "Google (with Auth0)");
+
+        }
+    }
+}
---
+++ MvcSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcSample.Startup))]
+namespace MvcSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
