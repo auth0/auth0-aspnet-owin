d546a6704fad42adc99730829943dd61b7a1f267
Sebastian Iacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-12-26 08:52:24 -0300

    minor

--- MvcSample/App_Start/Startup.Auth.cs
+++ MvcSample/App_Start/Startup.Auth.cs
@@ -34,14 +34,13 @@ namespace MvcSample
 
             //app.UseGoogleAuthentication();
 
-            // use Auth0
+            // add Google (with Auth0)
             app.AddAuth0Authentication(
                 clientId:       "YOUR_CLIENT_ID",
                 clientSecret:   "YOUR_CLIENT_SECRET", 
                 domain:         "YOUR_TENANT.auth0.com", 
                 //connection:   "google-oauth2", // OPTIONAL
                 displayName:    "Auth0");
-
         }
     }
 }
