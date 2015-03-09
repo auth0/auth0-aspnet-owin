27c088dd96d9e64b37b514bc3b5544330a3bf361
Sebastian Iacomuzzi <sebastian.iacomuzzi@gmail.com>
2015-03-09 13:13:39 -0300

    Update Startup.cs

--- examples/AspNet5-WebApp-OpenIdConnect-Sample/src/AspNet5.WebApp.OpenIdConnect/Startup.cs
+++ examples/AspNet5-WebApp-OpenIdConnect-Sample/src/AspNet5.WebApp.OpenIdConnect/Startup.cs
@@ -20,56 +20,56 @@ using System.Security.Principal;
 
 namespace AspNet5.WebApp.OpenIdConnect
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
     {
-        public Startup(IHostingEnvironment env)
-        {
-            // Setup configuration sources.
-            Configuration = new Configuration()
-                .AddJsonFile("config.json")
-                .AddEnvironmentVariables();
-        }
+      // Setup configuration sources.
+      Configuration = new Configuration()
+          .AddJsonFile("config.json")
+          .AddEnvironmentVariables();
+    }
 
-        public IConfiguration Configuration { get; set; }
+    public IConfiguration Configuration { get; set; }
 
-        // This method gets called by the runtime.
-        public void ConfigureServices(IServiceCollection services)
-        {
-			// Add MVC services to the services container.
-			services.AddMvc();
-
-			// OpenID Connect Authentication Requires Cookie Auth
+    // This method gets called by the runtime.
+    public void ConfigureServices(IServiceCollection services)
+    {
+    	// Add MVC services to the services container.
+    	services.AddMvc();
+    	
+    	// OpenID Connect Authentication Requires Cookie Auth
 			services.Configure<ExternalAuthenticationOptions>(options =>
 			{
 				options.SignInAsAuthenticationType = CookieAuthenticationDefaults.AuthenticationType;
 			});
-		}
+    }
 
-        // Configure is called after ConfigureServices is called.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
-        {
-            // Configure the HTTP request pipeline.
-            // Add the console logger.
-            loggerfactory.AddConsole();
+    // Configure is called after ConfigureServices is called.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
+    {
+      // Configure the HTTP request pipeline.
+      // Add the console logger.
+      loggerfactory.AddConsole();
 
-            // Add the following to the request pipeline only in development environment.
-            if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
-            {
-                app.UseBrowserLink();
-                app.UseErrorPage(ErrorPageOptions.ShowAll);
-                app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
-            }
-            else
-            {
-                // Add Error handling middleware which catches all application specific errors and
-                // send the request to the following path or controller action.
-                app.UseErrorHandler("/Home/Error");
-            }
+      // Add the following to the request pipeline only in development environment.
+      if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
+      {
+        app.UseBrowserLink();
+        app.UseErrorPage(ErrorPageOptions.ShowAll);
+        app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
+      }
+      else
+      {
+        // Add Error handling middleware which catches all application specific errors and
+        // send the request to the following path or controller action.
+        app.UseErrorHandler("/Home/Error");
+      }
 
-            // Add static files to the request pipeline.
-            app.UseStaticFiles();
+      // Add static files to the request pipeline.
+      app.UseStaticFiles();
 
-            // Configure the OWIN Pipeline to use OpenID Connect Authentication
+      // Configure the OWIN Pipeline to use OpenID Connect Authentication
 			app.UseCookieAuthentication(options => { });
 
 			app.UseOpenIdConnectAuthentication(options =>
@@ -94,19 +94,19 @@ namespace AspNet5.WebApp.OpenIdConnect
 						return Task.FromResult(0);
 					}
 				};
-            });
+      });
 
 			// Add MVC to the request pipeline.
 			app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action}/{id?}",
-                    defaults: new { controller = "Home", action = "Index" });
+      {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller}/{action}/{id?}",
+          defaults: new { controller = "Home", action = "Index" });
 
-                // Uncomment the following line to add a route for porting Web API 2 controllers.
-                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
-            });
-        }
+        // Uncomment the following line to add a route for porting Web API 2 controllers.
+        // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+      });
     }
+  }
 }
