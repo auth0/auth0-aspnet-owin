72de865d0babdc8bd20ccdcb51ee9b135ab36780
Sandrino Di Mattia <contact@sandrino.be>
2015-05-04 11:19:24 +0200

    Added RS256 example for Web Api

---
+++ examples/WebApi-RS256/Api/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web.Configuration;
+using System.Web.Http;
+
+using Api;
+
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var issuer = WebConfigurationManager.AppSettings["Auth0Domain"];
+            var audience = WebConfigurationManager.AppSettings["Auth0ClientID"];
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseActiveDirectoryFederationServicesBearerAuthentication(
+                new ActiveDirectoryFederationServicesBearerAuthenticationOptions
+                {
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = audience,
+                        ValidIssuer = issuer,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => parameters.IssuerSigningTokens.FirstOrDefault().SecurityKeys.FirstOrDefault()
+                    },
+                    MetadataEndpoint = String.Format("{0}/wsfed/{1}/FederationMetadata/2007-06/FederationMetadata.xml", issuer.TrimEnd('/'), audience)
+                });
+
+            var configuration = new HttpConfiguration();
+            configuration.MapHttpAttributeRoutes();
+            configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+            app.UseWebApi(configuration);
+        }
+    }
+}
