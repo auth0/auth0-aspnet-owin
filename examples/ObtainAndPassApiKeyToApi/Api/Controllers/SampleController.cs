0149155d008cae676065cd3e4e7e920fd6234d9c
Jerrrie Pelser <jerrie@jerriepelser.com>
2016-02-10 14:34:49 -0600

    Move auth0-aspnet-apikeys-sample repo into examples folder

---
+++ examples/ObtainAndPassApiKeyToApi/Api/Controllers/SampleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/sample")]
+    public class SampleController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<object> Get()
+        {
+            return (User.Identity as ClaimsIdentity).Claims.Select(c => new { c.Type, c.Value });
+        }
+    }
+}
---
+++ examples/ObtainAndPassApiKeyToApi/Client/ClaimItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Client
+{
+    public class ClaimItem
+    {
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+}
