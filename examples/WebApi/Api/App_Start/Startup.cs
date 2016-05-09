36e3481c44e1c23c107f578813485eb215559701
Jerrie Pelser <jerrie@jerriepelser.com>
2016-05-09 11:02:54 +0700

    Web API seed project now based on VS Web API project template

---
+++ examples/WebApi/Api/App_Start/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Api;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using WebConfigurationManager = System.Web.Configuration.WebConfigurationManager;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var issuer = WebConfigurationManager.AppSettings["Auth0Domain"];
+            var audience = WebConfigurationManager.AppSettings["Auth0ClientID"];
+            var secret = TextEncodings.Base64Url.Decode(
+                WebConfigurationManager.AppSettings["Auth0ClientSecret"]);
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] {audience},
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                    },
+                });
+        }
+    }
+}
--- examples/WebApi/Api/Properties/AssemblyInfo.cs
+++ examples/WebApi/Api/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Api")]
@@ -10,26 +10,26 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Api")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("16395c6c-e3ba-4dcb-824d-eecd490cbd2a")]
+[assembly: Guid("b57f2779-7853-467b-af3e-9d4c6bc9bea6")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
