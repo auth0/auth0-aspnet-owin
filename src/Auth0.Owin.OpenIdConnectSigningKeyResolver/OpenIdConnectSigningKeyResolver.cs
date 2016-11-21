ba558dc7af44945e78b8b4b46201d7abeadd7a60
Jerrie Pelser <jerrie@jerriepelser.com>
2016-11-21 16:36:23 +0700

    Add OpenIdConnectSigningKeyResolver

---
+++ src/Auth0.Owin.OpenIdConnectSigningKeyResolver/OpenIdConnectSigningKeyResolver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.IdentityModel.Protocols;
+
+namespace Auth0.Owin
+{
+    public class OpenIdConnectSigningKeyResolver
+    {
+        private readonly OpenIdConnectConfiguration openIdConfig;
+
+        public OpenIdConnectSigningKeyResolver(string authority)
+        {
+            var cm = new ConfigurationManager<OpenIdConnectConfiguration>($"{authority.TrimEnd('/')}/.well-known/openid-configuration");
+            openIdConfig = AsyncHelper.RunSync(async () => await cm.GetConfigurationAsync());
+        }
+
+        public SecurityKey GetSigningKey(SecurityKeyIdentifier identifier)
+        {
+            // Find the security token which matches the identifier
+            var securityToken = openIdConfig.SigningTokens.FirstOrDefault(t =>
+            {
+                // Each identifier has multiple clauses. Try and match for each
+                foreach (var securityKeyIdentifierClause in identifier)
+                {
+                    if (t.MatchesKeyIdentifierClause(securityKeyIdentifierClause))
+                        return true;
+                }
+
+                return false;
+            });
+
+            // Return the first key of the security token (if found)
+            return securityToken?.SecurityKeys.FirstOrDefault();
+        }
+    }
+}
---
+++ src/Auth0.Owin.OpenIdConnectSigningKeyResolver/readme.md
@@ -0,0 +1,22 @@
+﻿## Overview
+
+A helper class which will resolve the Issues Signin Key for JSON Web Tokens when using the standard OWIN JWT middleware. This is meant to be used when using RS256 signed JSON Web Tokens issued by Auth0.
+
+The public key will automatically be downloaded from the OIDC well-known endpoint.
+
+## Usage
+
+```
+var keyResolver = new OpenIdConnectSigningKeyResolver("https://your_tenant.auth0.com");
+app.UseJwtBearerAuthentication(
+    new JwtBearerAuthenticationOptions
+    {
+        AuthenticationMode = AuthenticationMode.Active,
+        TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidAudience = apiIdentifier,
+            ValidIssuer = domain,
+            IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => keyResolver.GetSigningKey(identifier)
+        }
+    });
+```
