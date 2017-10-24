b99a998389e772c9b10338bcd01539f5f520961a
Jerrie Pelser <jerrie@jerriepelser.com>
2017-10-24 14:48:13 +0700

    Remove examples folder

--- examples/MvcSample-ServerSideLogin/ServerSideLogin/Models/LoginSuccessViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ServerSideLogin.Models
-{
-    public class LoginSuccessViewModel
-    {
-        public string HtmlForm { get; set; }
-    }
-}
--- examples/basic-mvc-sample/BasicMvcSample/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasicMvcSample.MvcApplication" Language="C#" %>
